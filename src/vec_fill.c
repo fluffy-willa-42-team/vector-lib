d7e1f90421295cd68befa0866ca86c8b325fdff0
willaCS <arthur.a.j.willems@gmail.com>
2022-05-17 19:55:04 +0200

    start fill

--- src/vec_fill.c
+++ src/vec_fill.c
@@ -6,12 +6,33 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:45:50 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/17 16:20:51 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/17 19:43:20 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector-template.h"
 
+#define VEC_DEFAULT		0
+#define VEC_SKIP		0b0001
+#define VEC_SEPATOR		0b0010
+#define VEC_MULTIPLE	0b0100
+
 t_vec	*vecFill(t_vec *vec){
+	int option;
+	
+	option = (VEC_SKIP | VEC_SEPATOR);
+	
+	if (option & VEC_SKIP){
+		printf("WITH SKIPS\n");
+	}
+
+	if (option & VEC_SEPATOR){
+		printf("WITH SEPARATOR\n");
+	}
+
+	if (option & VEC_MULTIPLE){
+		printf("WITH MULTIPLE\n");
+	}
+
 	return (vec);
 }
