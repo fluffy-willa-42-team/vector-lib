8e8e6fc59409e3b7d3900269bc7cd68dedc51511
Matthew-Dreemurr <hadadmat@gmail.com>
2022-05-17 15:45:10 +0200

    create base

--- src/vec_init.c
+++ src/vec_init.c
@@ -6,16 +6,29 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:34:34 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/17 15:36:39 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/17 15:44:07 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector-lib.h"
 
-void vecInitStrict(void){
-	return ;
+static t_vec	vecEmpty(void)
+{
+	t_vec	res;
+
+	res.buffer = NULL;
+	res.len = 0;
+	res.size = 0;
+	return (res);
 }
 
-void vecInit(void){
-	return ;
+t_vec vecInitStrict(int size){
+	t_vec	res;
+	
+	res = vecEmpty();
+	return (res);
+}
+
+t_vec vecInit(void){
+	return (vecInitStrict(VECTOR_SIZE));
 }
