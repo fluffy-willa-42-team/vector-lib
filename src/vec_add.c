d0ba47cb1e89b059e620ecd8579c04ec4fc05285
willaCS <arthur.willems.prive@gmail.com>
2022-05-24 12:30:34 +0200

    test

--- src/vec_add.c
+++ src/vec_add.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 11:17:03 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/24 11:58:45 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/24 12:28:24 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 #include <stdio.h>
 
 t_vec	*vec_resize(t_vec *vec);
+
+int	is_zero(void *mem_raw, int size)
+{
+	unsigned char	*mem;
+	int				i;
+
+	mem = (unsigned char *) mem_raw;
+	i = 0;
+	while (i < size)
+	{
+		if (mem[i] != 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 /**
  * @brief [internal] Get the first writable character inside the vector and will
@@ -27,7 +43,7 @@ static int	get_start_vec(t_vec *vec)
 	if (!vec->buffer)
 		vec_resize(vec);
 	res = vec->len - 1;
-	while (res > 0 && vec->buffer[res - 1]) //TODO 
+	while (res > 0 && is_zero(vec->buffer, vec->size))
 		res--;
 	while (vec->len < res)
 		vec_resize(vec);
