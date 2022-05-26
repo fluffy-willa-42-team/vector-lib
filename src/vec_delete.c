b354ba99abc807ce17c817614797f4744c11ec4b
matthew-dreemurr <hadadmat@gmail.com>
2022-05-26 10:11:13 +0200

    added description
    Co-authored-by: Fluffy_Kaiju <Matthew-Dreemurr@users.noreply.github.com>

--- src/vec_delete.c
+++ src/vec_delete.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 19:22:33 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/18 14:32:54 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/26 10:01:49 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 
+/**
+ * @brief Free the vector and set `vec->len` to `0`.
+ */
 void	vec_delete(t_vec *vec)
 {
 	vec->len = 0;
 	free(vec->buffer);
 }
 
+/**
+ * @brief Set all buffer to `\0`.
+ */
 void	vec_delete_content(t_vec *vec)
 {
 	ft_memset(vec->buffer, vec->len, vec->size);
