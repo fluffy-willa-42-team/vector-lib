4923ecf2f561e9c2899f707f8bd395214f2c36b3
willaCS <arthur.willems.prive@gmail.com>
2022-05-30 10:37:33 +0200

    changed insert

--- src/vec_insert.c
+++ src/vec_insert.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 11:53:18 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/24 11:46:41 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/30 10:36:00 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		g_start_vec(t_vec *vec, int str_len, int sep_len, char *sep);
+
+t_vec	*vec_resize(t_vec *vec);
 
 t_vec	*vec_insert(t_vec *vec, char *str, int index)
 {
@@ -28,6 +30,8 @@ t_vec	*vec_insert(t_vec *vec, char *str, int index)
 		return (vec);
 	len = ft_strlen(str);
 	start = g_start_vec(vec, len, 0, NULL);
+	while (vec->len < start + len + 1)
+		vec_resize(vec);
 	ft_memmove(vec->buffer + index + len, vec->buffer + index, vec->size - len);
 	ft_memmove(vec->buffer + index, str, len);
 	return (vec);
