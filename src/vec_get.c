1f8bc2420d0c58b10e72fba8c3ef599f16cd1508
willaCS <arthur.willems.prive@gmail.com>
2022-05-31 08:16:41 +0200

    bugfix

--- src/vec_get.c
+++ src/vec_get.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:33:19 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/30 15:49:43 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/31 08:15:42 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ int	get_index(int index, int len);
 
 void	*vec_get(t_vec *vec, int index)
 {
+	if (vec->content_len == 0)
+		return (NULL);
 	return (vec->buffer + get_index(index, vec->content_len) * vec->size);
 }
