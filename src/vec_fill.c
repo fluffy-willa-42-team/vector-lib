cf72ac42a3ca8ab5840432cb47c7a21253b08818
willaCS <arthur.willems.prive@gmail.com>
2022-05-31 09:30:32 +0200

    continue insert

--- src/vec_fill.c
+++ src/vec_fill.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:45:50 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/31 08:40:40 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/31 09:30:17 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ typedef struct s_vf_option
 	char	*multi_sep;
 	size_t	multi_sep_len;
 	size_t	nb;
+	size_t	i;
+	char	*str;
+	size_t	len;
 }	t_vf_option;
 
 static void	init_sep_opt(t_vf_option *opt, int option, char **ptr, size_t *len)
@@ -65,26 +68,23 @@ static void	add_elem_in_vec(t_vec *vec, char *src, size_t len)
 t_vec	*vec_fill(t_vec *vec, t_fill_opt option, ...)
 {
 	t_vf_option	opt;
-	size_t		i;
-	char		*str;
-	size_t		len;
 
 	va_start(opt.arg_list, option);
 	init_option(&opt, option);
 	if (vec->content_len != 0)
 		add_elem_in_vec(vec, opt.sep, opt.sep_len);
-	i = 0;
-	while (i < opt.nb)
+	opt.i = 0;
+	while (opt.i < opt.nb)
 	{
-		str = va_arg(opt.arg_list, char *);
+		opt.str = va_arg(opt.arg_list, char *);
 		if (option & FIXED_LEN)
-			len = va_arg(opt.arg_list, int);
+			opt.len = va_arg(opt.arg_list, int);
 		else
-			len = ft_strlen(str);
-		if (i != 0)
+			opt.len = ft_strlen(opt.str);
+		if (opt.i != 0)
 			add_elem_in_vec(vec, opt.multi_sep, opt.multi_sep_len);
-		add_elem_in_vec(vec, str, len);
-		i++;
+		add_elem_in_vec(vec, opt.str, opt.len);
+		opt.i++;
 	}
 	va_end(opt.arg_list);
 	return (vec);
--- src/vec_insert.c
+++ src/vec_insert.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 08:20:03 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/31 08:56:48 by awillems         ###   ########.fr       */
+/*   Updated: 2022/05/31 09:25:13 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,22 @@
 
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int		get_index(int index, int len);
 
 t_vec	*vec_resize_round(t_vec *vec, size_t new_len);
 
 typedef struct s_vi_option {
 	va_list	arg_list;
+	size_t	index;	
 	size_t	option;
 	char	*sep;
 	size_t	sep_len;
 	char	*multi_sep;
 	size_t	multi_sep_len;
 	size_t	nb;
+	char	*str;
+	size_t	len;
+	size_t	i;
 }	t_vi_option;
 
 static void	init_sep_opt(t_vi_option *opt, int option, char **ptr, size_t *len)
@@ -46,10 +51,11 @@ static void	init_sep_opt(t_vi_option *opt, int option, char **ptr, size_t *len)
 	*len = 1;
 }
 
-static void	init_option(t_vi_option *opt, size_t option)
+static void	init_option(t_vec *vec, t_vi_option *opt, size_t option)
 {
 	opt->nb = 1;
 	opt->option = option;
+	opt->index = get_index(va_arg(opt->arg_list, int), vec->content_len);
 	init_sep_opt(opt, SEP, &opt->sep, &opt->sep_len);
 	init_sep_opt(opt, SEP, &opt->multi_sep, &opt->multi_sep_len);
 	if (option & MULTI)
@@ -61,10 +67,25 @@ t_vec	*vec_insert(t_vec *vec, t_fill_opt option, ...)
 	t_vi_option	opt;
 	
 	va_start(opt.arg_list, option);
-	init_option(&opt, option);
-	printf("%zu\n", opt.nb);
-	printf("%s (%zu)\n", opt.sep, opt.sep_len);
-	printf("%s (%zu)\n", opt.multi_sep, opt.multi_sep_len);
+	init_option(vec, &opt, option);
+	printf("%zu => %zu\n", opt.nb, opt.index);
+	opt.i = 0;
+	if (opt.index != 0)
+		printf("%s (%zu)\n", opt.sep, opt.sep_len);
+	while (opt.i < opt.nb)
+	{
+		if (opt.i != 0)
+			printf("%s (%zu)\n", opt.multi_sep, opt.multi_sep_len);
+		opt.str = va_arg(opt.arg_list, char *);
+		if (option & FIXED_LEN)
+			opt.len = va_arg(opt.arg_list, int);
+		else
+			opt.len = ft_strlen(opt.str);
+		printf("%zu => %s (%zu)\n", opt.i, opt.str, opt.len);
+		opt.i++;
+	}
+	if (opt.index != vec->content_len - 1)
+		printf("%s (%zu)\n", opt.sep, opt.sep_len);
 	va_end(opt.arg_list);
 	return (vec);
 }
