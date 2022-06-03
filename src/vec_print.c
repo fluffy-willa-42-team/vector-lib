12b3d1948d1743199e0a38adb5f284413d606cb7
willaCS <arthur.willems.prive@gmail.com>
2022-06-03 12:08:20 +0200

    print ++

--- src/vec_print.c
+++ src/vec_print.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:58:32 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/01 10:27:44 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/03 12:07:36 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	vec_print(t_vec *vec)
 	if (!vec || !vec->buffer)
 		return ;
 	i = 0;
-	while (i < vec->len)
+	while (i < vec->len * vec->size)
 	{
 		if (ft_isprint(((char *) vec->buffer)[i]))
 			write(1, &((char *) vec->buffer)[i], 1);
@@ -31,6 +31,8 @@ void	vec_print(t_vec *vec)
 			write(1, "\e[0;31m.\e[0m", 13);
 		else
 			write(1, "\e[0;36m.\e[0m", 13);
+		if (vec->size > 1 && ((i + 1) % vec->size == 0))
+			write(1, "\n", 1);
 		i++;
 	}
 	write(1, "\n", 1);
