0d11a6ac6baf43f965a5a8bf45da23de7dcc3958
willaCS <arthur.willems.prive@gmail.com>
2022-06-06 13:53:42 +0200

    fixed insert bug

--- src/utils/get_index.c
+++ src/utils/get_index.c
@@ -6,14 +6,14 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 09:12:02 by awillems          #+#    #+#             */
-/*   Updated: 2022/05/30 15:45:23 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/06 13:52:44 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	get_index(int index, int len)
 {
-	if (len < 1)
-		return (-1);
+	if (len == 0)
+		return (0);
 	while (index < 0)
 		index += len;
 	while (index >= len)
