9e74e2d8d0af8622433fddabdf54efc20ee3fea7
willaCS <arthur.willems.prive@gmail.com>
2022-06-14 12:25:04 +0200

    wip

--- src/vec_cast.c
+++ src/vec_cast.c
@@ -14,17 +14,34 @@
 
 #include "stdio.h"
 
+size_t	ft_strlen(const char *s);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 t_vec	*vec_resize_round(t_vec *vec, size_t new_len);
 
 t_vec	*vec_cast(t_vec *vec, size_t new_size, void *casting_function(void *))
 {
-	const size_t old_size = vec->size;
-	(void) vec;
-	(void) new_size;
 	(void) casting_function;
-	(void) old_size;
-	printf("new size : %zu\n", new_size);
 	if (vec->content_len * new_size >= vec->len)
 		vec_resize_round(vec, vec->content_len * new_size);
+	if (new_size > vec->size)
+	{
+		printf("%lu > %lu (<=)\n", new_size, vec->size);
+		for (size_t i = 0; i < vec->content_len; i++)
+		{
+			ft_memmove(vec->buffer + (vec->content_len - i - 1) * new_size,
+				casting_function(vec->buffer + i * vec->size),
+				new_size);
+		}
+	}
+	else
+	{
+		printf("%lu <= %lu (=>)\n", new_size, vec->size);
+		for (size_t i = 0; i < vec->content_len; i++)
+		{
+			ft_memmove(vec->buffer + i * new_size,
+				casting_function(vec->buffer + i * vec->size),
+				new_size);
+		}
+	}
 	return (vec);
 }
