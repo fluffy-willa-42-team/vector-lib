92fb742f533f11976602d6857936b2011676067b
willaCS <arthur.willems.prive@gmail.com>
2022-06-28 11:09:42 +0200

    [FIX] cast should be could good

--- src/vec_cast.c
+++ src/vec_cast.c
@@ -6,7 +6,7 @@
 /*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:45:50 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/28 11:00:27 by awillems         ###   ########.fr       */
+/*   Updated: 2022/06/28 11:07:53 by awillems         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,25 +45,12 @@ t_vec	*vec_cast(t_vec *vec, size_t new_size, int casting_function())
 
 	if (!vec_resize_cast(vec, new_size))
 		return (NULL);
-	if (new_size <= old_size)
+	i = (int[]){vec->content_len - 1, 0}[new_size <= old_size];
+	while ((int[]){i >= 0, i < (long) vec->content_len}[new_size <= old_size])
 	{
-		i = 0;
-		while (i < (long) vec->content_len)
-		{
-			if (!casting_function(vec->buffer + i * old_size, vec->buffer + i * new_size))
-				return (NULL);
-			i++;
-		}
-	}
-	else
-	{
-		i = vec->content_len - 1;
-		while (i >= 0)
-		{
-			if (!casting_function(vec->buffer + i * old_size, vec->buffer + i * new_size))
-				return (NULL);
-			i--;
-		}
+		if (!casting_function(vec->buffer + i * old_size, vec->buffer + i * new_size))
+			return (NULL);
+		i += (int[]){-1, 1}[new_size <= old_size];
 	}
 	if (new_size <= old_size)
 		ft_memset(vec->buffer + vec->content_len * new_size, 0,
