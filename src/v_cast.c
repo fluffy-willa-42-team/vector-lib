d8b46cea148f60d5e66e86a6bd57a7da6fdc1268
willaCS <arthur.willems.prive@gmail.com>
2022-10-11 14:23:45 +0200

    added more setup for readline and cast

---
+++ src/v_cast.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   v_cast.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awillems <awillems@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 14:21:31 by awillems          #+#    #+#             */
+/*   Updated: 2022/10/11 14:21:52 by awillems         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vector_template.h"
+
+t_vec	*v_cast(t_vec *vec, size_t new_size, int casting_function())
+{
+	(void) casting_function;
+	(void) new_size;
+	return (vec);
+}
